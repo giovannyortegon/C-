6feab1652606e589a63bbff2a1e5eed1213b8a88
giovannyortegon <ortegon.giovanny@hotmail.com>
2022-08-11 13:20:52 -0500

    [ADD] creating frist project and using Input and Output in console

---
+++ OpenBootCamp/Chap01/Saludo/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escriba su nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hello, {0}", nombre);
+Console.WriteLine("La hora actual es: {0}", DateTime.Now);
---
+++ README.md
@@ -0,0 +1,1 @@
+C# Languge with Open Bootcamp
