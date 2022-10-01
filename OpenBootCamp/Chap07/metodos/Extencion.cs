a8f12122fa86b47cf20c5e07b23975d26e7ab3a9
giovannyortegon <ortegon.giovanny@hotmail.com>
2022-10-01 00:04:42 -0500

    [ADD] metodos

---
+++ OpenBootCamp/Chap07/metodos/Extencion.cs
@@ -0,0 +1,7 @@
+namespace Extension {
+    internal class ExtensionMotos {
+        public void Motos() {
+            Console.WriteLine("Extension.");
+        }
+    } 
+}
---
+++ OpenBootCamp/Chap07/metodos/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Extension ;
+
+Console.WriteLine("********** Metodos **********");
+
+
+Motos m1 = new Motos("Auteco","Cross");
+m1.Arrancar(); 
+Console.WriteLine("m1: " + m1.ToString());
+
+
+class Motos {
+
+    private string? name;
+    private string? marca;
+
+    public Motos() {}
+    public Motos(string name, string marca) {
+        this.name = name;
+        this.marca = marca;
+    }
+
+    public void Arrancar(){
+        Console.WriteLine("Arrancando...");
+    }
+
+    public override string ToString() {
+        return $"Nombre: {this.name} Marca {this.marca} \n";
+    }
+}
